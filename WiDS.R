a67c1a60703e24dfe3bc17bcb442021fd46617a9
Maggie Church <49007067+maggiechurch741@users.noreply.github.com>
2019-04-09 00:02:05 -0400

    Update WiDS.R with choro-making fn

--- WiDS.R
+++ WiDS.R
@@ -2,36 +2,42 @@ library(tidyverse)
 library(choroplethr)
 library(choroplethrMaps)
 
-# Data comes from the 2013 5-year American Community Survey (ACS).
-data(df_county_demographics)
-
-demog_choro <- df_county_demographics %>%
-  select(region, percent_black) %>%
-  rename("value" = percent_black)
-
 # Breast Cancer Data from CDC 
 cdc <- read.delim("/fsr/home/m1mgc02/Personal/wids_data_challenge_2019-master/Compressed Mortality, 1999-2016.txt") 
 
-# crude rate = death/population * 100,000. Age adjustment is a technique for "removing" the effects of age from crude rates.
-# Rates are marked as "unreliable" when the death count is less than 20.
+# Crude rate = death/population * 100,000. Age adjustment is a technique 
+# for "removing" the effects of age from crude rates.
 
 
-# Clean data
+# Create a new variable for "Unreliable" data (when the death count is less than 20)
 cdc_clean <- cdc %>% 
   mutate(unreliable = str_detect(`Crude.Rate`, "Unreliable")) %>%
   mutate(Crude.Rate = str_replace(`Crude.Rate`, "\\(Unreliable\\)", "")) %>%
   mutate(Crude.Rate = as.numeric(`Crude.Rate`)) %>%
   mutate(Age.Adjusted.Rate = str_replace(`Age.Adjusted.Rate`, "\\(Unreliable\\)", "")) %>%
-  mutate(Age.Adjusted.Rate = as.numeric(`Age.Adjusted.Rate`))
+  mutate(Age.Adjusted.Rate = as.numeric(`Age.Adjusted.Rate`)) %>%
 
-# Format data for Choroplethr
-cdc_choro <- cdc_clean %>% 
-  filter(!is.na(Age.Adjusted.Rate)) %>%
-  rename("value" = Age.Adjusted.Rate) %>%
-  rename("region" = County.Code) %>%
-  select(value, region)
+  # Rename region for ease of the merge and plotting
+  rename(region = "County.Code")
 
-county_choropleth(demog_choro)
-county_choropleth(cdc_choro)
+# Data comes from the 2013 5-year American Community Survey (ACS).
+data(df_county_demographics)
+
+# Merge CDC with demographic data
+cdc_full <- inner_join(cdc_clean, df_county_demographics, by = "region")
+
+# Map Choroplethr
+choro <- function(df, var){
+  var.enquo <- enquo(var)
+  
+  df %>% 
+    filter(!is.na(!!var.enquo)) %>%
+    rename("value" = !!var.enquo) %>%
+    select(value, region) %>%
+    county_choropleth() 
+}
+
+cdc_full %>% choro(Age.Adjusted.Rate) 
+
 
 
